3abb2646bdfa4e2c6341915b0d34a75780ab1d15
tdnguyen <tdnguyen.uet@gmail.com>
2020-05-26 21:21:22 +0700

    fund service implementation

--- src/main/java/com/example/ConsumerManagement/models/repositories/FundRepository.java
+++ src/main/java/com/example/ConsumerManagement/models/repositories/FundRepository.java
@@ -1,7 +1,15 @@
 package com.example.ConsumerManagement.models.repositories;
 
 import com.example.ConsumerManagement.models.persistence.entities.Fund;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FundRepository extends CrudRepository<Fund, Integer> {
+    @Query("SELECT owner from Fund where fundId = :fundId")
+    String findOwnerByFundId(int fundId);
+
+    @Modifying
+    @Query(value = "update Fund set owner = :newOwner where fundId = :fundId")
+    void changeOwner(int fundId, String newOwner);
 }
