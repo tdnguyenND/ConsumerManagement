c353d34298793a1a1b90cda743de5a4a266385dc
TrangTrinh <trinhtrang27112000@gmail.com>
2020-05-29 12:24:16 +0700

    completed and tested createTransactionController class

--- src/main/java/com/example/ConsumerManagement/controllers/transaction/CreateTransactionController.java
+++ src/main/java/com/example/ConsumerManagement/controllers/transaction/CreateTransactionController.java
@@ -1,4 +1,34 @@
 package com.example.ConsumerManagement.controllers.transaction;
 
+import com.example.ConsumerManagement.models.persistence.entities.Transaction;
+import com.example.ConsumerManagement.services.TransactionService;
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
 public class CreateTransactionController {
+    @Autowired
+    TransactionService transactionService;
+
+    @PostMapping(value = "/transaction", consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> create(@ModelAttribute("transaction") Transaction transaction, HttpServletResponse res){
+        LocalDate dateNow = LocalDate.now();
+        String dateOfCreation = dateNow.toString();
+
+        transaction.setDateOfCreation(dateOfCreation);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.save(transaction));
+    }
 }
--- src/test/java/com/example/ConsumerManagement/controllers/transaction/CreateTransactionControllerTest.java
+++ src/test/java/com/example/ConsumerManagement/controllers/transaction/CreateTransactionControllerTest.java
@@ -1,5 +1,55 @@
+package com.example.ConsumerManagement.controllers.transaction;
+
+import com.example.ConsumerManagement.models.persistence.entities.Transaction;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.awt.*;
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CreateTransactionControllerTest {
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    @Test
+    void create() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        String uri = "/transaction";
+
+        LocalDate dateNow = LocalDate.now();
+        String dateOfCreation = dateNow.toString();
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .param("fundId", "5")
+                .param("name", "tien nha")
+                .param("type", "reduce")
+                .param("actor", "Trinh Trang")
+                .param("amountOfMoney", "1000000")
+                .param("note", "chat vcl")
+                .param("dateOfCreation", dateOfCreation))
+                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertTrue(content.matches("\\{\"transactionId\":\\d+,\"fundId\":5,\"name\":\"tien nha\",\"type\":\"reduce\",\"actor\":\"Trinh Trang\",\"amountOfMoney\":1000000.0,\"note\":\"chat vcl\",\"dateOfCreation\":\"2020-05-29\"}"));
+    }
 }
